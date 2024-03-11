122bab5a66e8b529cbe994a69427051e528c6fa3
4BigTasty4 <eldanizyolchuyev@gmail.com>
2024-03-11 21:11:31 +0400

    HM1
    HM1

---
+++ HM1/HM1/Program.cs
@@ -0,0 +1,48 @@
+﻿//AutoResetEvent 
+
+// class Program
+// {
+//     static AutoResetEvent resetEvent = new AutoResetEvent(false);
+//
+//     static void Main(string[] args)
+//     {
+//         Thread th = new Thread(Status);
+//
+//         th.Start();
+//         resetEvent.Set();
+//         th.Join();
+//     }
+//
+//     static void Status()
+//     {
+//         Console.WriteLine("The worker thread is waiting...");
+//         resetEvent.WaitOne();
+//         Console.WriteLine("Workflow has resumed!");
+//     }
+// }
+
+
+//ManualResetEvent 
+
+// class Program
+// {
+//     static ManualResetEvent resetEvent = new ManualResetEvent(false);
+//
+//     static void Main(string[] args)
+//     {
+//         Thread th = new Thread(Status);
+//
+//         th.Start();
+//         resetEvent.Set();
+//         Thread.Sleep(1000);
+//         resetEvent.Reset();
+//         th.Join();
+//     }
+//
+//     static void Status()
+//     {
+//         Console.WriteLine("The worker thread is waiting...");
+//         resetEvent.WaitOne();
+//         Console.WriteLine("Workflow has resumed!");
+//     }
+// }
